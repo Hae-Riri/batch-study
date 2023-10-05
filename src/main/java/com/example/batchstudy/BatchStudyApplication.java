c991ce2be3b832989c69dda56e1e0cebec6f5a9d
Hae-Riri <ehl3288@naver.com>
2023-10-05 18:05:25 +0900

    init

--- src/main/java/com/example/batchstudy/BatchStudyApplication.java
+++ src/main/java/com/example/batchstudy/BatchStudyApplication.java
@@ -1,9 +1,11 @@
 package com.example.batchstudy;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchStudyApplication {
 
     public static void main(String[] args) {
